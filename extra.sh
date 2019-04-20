6f882a3352cda07c8cdc243988f27e6706cbd7ed
Calvin Collins <calvinc@surveymonkey.com>
2019-04-20 16:06:00 -0400

    ruby

--- extra.sh
+++ extra.sh
@@ -1,6 +1,4 @@
-rbenv init
-zsh
-rbenv install $(rbenv install -l | grep -v - | tail -1)
+export PATH=/usr/local/opt/ruby/bin:$PATH
 gem install tmuxinator
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
