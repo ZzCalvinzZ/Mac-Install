977f06409e4d8b9124ccb4c5383df1e20ae61e92
Calvin Collins <calvinc@surveymonkey.com>
2019-04-19 22:29:59 -0400

    try upfront

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,3 +1,9 @@
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 echo "enter the email you would like to use for your ssh keys"
 read keyaddr
 ssh-keygen -t rsa -b 4096 -C "$keyaddr"
